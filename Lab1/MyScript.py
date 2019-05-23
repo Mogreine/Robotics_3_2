bdd728810666bd10e7bd8dd354d7ffd5a60ab0a7
Mogreine <north7311@gmail.com>
2019-05-23 23:35:16 +0400

    Changed consts back

--- Lab1/MyScript.py
+++ Lab1/MyScript.py
@@ -13,8 +13,8 @@ class Robot:
     e = 0
     prev_e = 0
     iSum = 0
-    initial_speed = 0.5
-    maintained_dist_right = 0.6
+    initial_speed = 0.2
+    maintained_dist_right = 0.4
 
     def __init__(self):
         vrep.simxFinish(-1)
@@ -29,30 +29,30 @@ class Robot:
 
         error_code, self.left_motor_handle = vrep.simxGetObjectHandle(self.client_id, 'Pioneer_p3dx_leftMotor',
                                                                       vrep.simx_opmode_oneshot_wait)
-        self.check_on_error(error_code, "Couldn't find left motor!", True)
+        self.check_error(error_code, "Couldn't find left motor!", True)
 
         error_code, self.right_motor_handle = vrep.simxGetObjectHandle(self.client_id, 'Pioneer_p3dx_rightMotor',
                                                                        vrep.simx_opmode_oneshot_wait)
-        self.check_on_error(error_code, "Couldn't find right motor!", True)
+        self.check_error(error_code, "Couldn't find right motor!", True)
 
         error_code, self.laser_left = vrep.simxGetObjectHandle(self.client_id, 'Pr_sensor_left',
                                                                vrep.simx_opmode_oneshot_wait)
-        self.check_on_error(error_code, "Couldn't find left laser!", True)
+        self.check_error(error_code, "Couldn't find left laser!", True)
         error_code, self.laser_right = vrep.simxGetObjectHandle(self.client_id, 'Pr_sensor_right',
                                                                 vrep.simx_opmode_oneshot_wait)
-        self.check_on_error(error_code, "Couldn't find right laser!", True)
+        self.check_error(error_code, "Couldn't find right laser!", True)
         error_code, self.laser_front = vrep.simxGetObjectHandle(self.client_id, 'Pr_sensor_front',
                                                                 vrep.simx_opmode_oneshot_wait)
-        self.check_on_error(error_code, "Couldn't find front laser!", True)
+        self.check_error(error_code, "Couldn't find front laser!", True)
         error_code, self.laser_middle = vrep.simxGetObjectHandle(self.client_id, 'Pr_sensor_middle',
                                                                  vrep.simx_opmode_oneshot_wait)
-        self.check_on_error(error_code, "Couldn't find middle laser!", True)
+        self.check_error(error_code, "Couldn't find middle laser!", True)
 
         sec, msec = vrep.simxGetPingTime(self.client_id)
         print("Ping time: %f" % (sec + msec / 1000.0))
         print('Initialization is finished.')
 
-    def check_on_error(self, e, msg, need_exit=False):
+    def check_error(self, e, msg, need_exit=False):
         if e != vrep.simx_return_ok:
             print(f'Error: {msg}')
             if need_exit:
@@ -61,32 +61,28 @@ class Robot:
     def set_motor_speed(self, left_speed, right_speed):
         e = vrep.simxSetJointTargetVelocity(self.client_id, self.left_motor_handle, left_speed,
                                             vrep.simx_opmode_oneshot_wait)
-        self.check_on_error(e, "SetJointTargetVelocity for left motor got error code")
+        self.check_error(e, "SetJointTargetVelocity for left motor got error code")
         e = vrep.simxSetJointTargetVelocity(self.client_id, self.right_motor_handle, right_speed,
                                             vrep.simx_opmode_oneshot_wait)
-        self.check_on_error(e, "SetJointTargetVelocity for right motor got error code")
+        self.check_error(e, "SetJointTargetVelocity for right motor got error code")
         print(f"Motor's speed is set to {left_speed} {right_speed}")
 
-    def fix_distance(self, dist, old_dist):
+    def pid(self, dist, old_dist):
         # constants
-        kp = 0.4
-        kd = 1
-        ki = 0.1
-        iMin, iMax = -0.2, 0.2
+        kp = 0.5
+        kd = 0.8
+        ki = 0.12
+        iMin, iMax = -0.25, 0.25
 
-        # error calculation
         self.e = self.maintained_dist_right - dist
-
-        # Prop
         up = kp * self.e
-
-        # Diff
         ud = kd * (dist - old_dist)
 
-        # Integral
         self.iSum += self.e
-        self.iSum = max(iMin, self.iSum)
-        self.iSum = min(iMax, self.iSum)
+        if self.iSum < iMin:
+            self.iSum = iMin
+        if self.iSum > iMax:
+            self.iSum = iMax
         ui = ki * self.iSum
 
         res = up + ud + ui
@@ -129,19 +125,18 @@ class Robot:
             elif not left_detection and not right_detection and middle_detection:
                 dist = distance_middle
             elif not left_detection:
-                print("Turning")
-                dist = 0.7
+                dist = 0.6
                 if middle_detection:
                     dist = min(dist, distance_middle, distance_right)
             elif left_detection and middle_detection:
                     dist = min(distance_left, distance_middle)
 
             if front_detection:
-                dist = min(distance_front, dist)
+                dist = min(distance_front / 2, dist)
             print("Distance: {}".format(dist))
 
             err_arr_tmp[counter] = self.maintained_dist_right - dist
-            left_arr_tmp[counter], right_arr_tmp[counter] = self.fix_distance(dist, prev_dist)
+            left_arr_tmp[counter], right_arr_tmp[counter] = self.pid(dist, prev_dist)
 
             counter += 1
             if counter == 100:
@@ -162,22 +157,22 @@ class Robot:
         (e, left_detection, left, detectedObjectHandle,
          detectedSurfaceNormalVector) = vrep.simxReadProximitySensor(self.client_id, self.laser_left,
                                                                      vrep.simx_opmode_buffer)
-        self.check_on_error(e, "Left sensor data reading error")
+        self.check_error(e, "Left sensor data reading error")
 
         (e, right_detection, right, detectedObjectHandle,
          detectedSurfaceNormalVector) = vrep.simxReadProximitySensor(self.client_id, self.laser_right,
                                                                      vrep.simx_opmode_buffer)
-        self.check_on_error(e, "Right sensor data reading error")
+        self.check_error(e, "Right sensor data reading error")
 
         (e, front_detection, front, detectedObjectHandle,
          detectedSurfaceNormalVector) = vrep.simxReadProximitySensor(self.client_id, self.laser_front,
                                                                      vrep.simx_opmode_buffer)
-        self.check_on_error(e, "Front sensor data reading error")
+        self.check_error(e, "Front sensor data reading error")
 
         (e, middle_detection, middle, detectedObjectHandle,
          detectedSurfaceNormalVector) = vrep.simxReadProximitySensor(self.client_id, self.laser_middle,
                                                                      vrep.simx_opmode_buffer)
-        self.check_on_error(e, "Middle sensor data reading error")
+        self.check_error(e, "Middle sensor data reading error")
 
         return front_detection, left_detection, middle_detection, right_detection, np.linalg.norm(front), np.linalg.norm(left), np.linalg.norm(middle), np.linalg.norm(right)
 
@@ -190,16 +185,16 @@ if __name__ == '__main__':
     plt.figure(1)
     plt.subplot(211)
     plt.plot(x, err_arr)
-    plt.title('Error change')
-    plt.ylabel('Error')
-    plt.xlabel('Time, sec')
+    plt.title('Изменение')
+    plt.ylabel('Ошибка')
+    plt.xlabel('Время, с')
     plt.grid(True)
 
     plt.subplot(212)
     plt.plot(x, left, x, right)
-    plt.title('Velocity change (blue - left, orange - right)')
-    plt.ylabel('Velocity')
-    plt.xlabel('Time, sec')
+    plt.title('Изменение скорости (синий - левое, оранжевый - правое)')
+    plt.ylabel('Скорость')
+    plt.xlabel('Время, с')
     plt.grid(True)
 
     plt.tight_layout()
